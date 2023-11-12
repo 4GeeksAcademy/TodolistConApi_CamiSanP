3b474a569a9f9f0869ba31d16367e8af2d914ce8
CamiSanP <145134410+CamiSanP@users.noreply.github.com>
2023-11-12 02:25:29 +0000

    [Todolist]

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 const Home = () => {
 	const [value, setValue] = useState("");
 	const [todos, setTodos] = useState([]);
+	const [delet, setDelet] = useState(false);
 	return (
 		<div className="Todolist">
 			<h1 className="myh1">Todos</h1>
@@ -12,10 +13,13 @@ const Home = () => {
 				<input type="text" className="input" onChange={(e) => setValue(e.target.value)} value={value} placeholder="What needs to be done?" onKeyDown={(e) => { if (e.key === "Enter") { setTodos(todos.concat([value])); setValue(""); } }} />
 
 				{todos.map((item, index) => (
-					<li className="items">{item}{" "} <i className="fa-solid fa-xmark" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
-					</li>
+					<li key={index} className="items" onMouseEnter={() => setDelet(index)} onMouseLeave={() => setDelet(null)}> {item}{" "}{delet === index && (
+					  <i className="fa-solid fa-xmark" onClick={() => setTodos(todos.filter((_, indexActual) => index !== indexActual))}></i>
+					)}
+				  </li>
 				))}
 			</ul>
+			<div style={{paddingLeft:"30px", color:" rgba(62, 0, 128, 0.384)"}}>{todos.length > 0 ? (<p>{todos.length} item left</p>) : (<p>Add task</p>)}</div>
 		</div>
 
 	);
